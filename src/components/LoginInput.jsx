b52079c3a6adf1119b6622eafa19e0da099d436c
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2023-02-27 12:55:28 +0100

    page style

---
+++ src/components/LoginInput.jsx
@@ -0,0 +1,33 @@
+import '../Styles/loginInput.css'
+import facebookIcon from '../assets/facebook.png'
+const LoginInput = () => {
+    return(
+    <div className="input-box">
+        <div>
+            <input type="email" autoComplete='username' placeholder='Numero di telefono, nome utente o e-mail' />
+        </div>
+
+        <div>
+            <input type="password" autoComplete='current-password' placeholder='Password' />
+        </div>
+
+        <button>Accedi</button>
+
+        <div className='border-container'>
+            <div className='border'></div>
+            <div><p>o</p></div>
+            <div className='border'></div>
+        </div>
+        
+        <div className='facebook-login'>
+            <img src={facebookIcon} alt="" />
+            <p>Accedi con Facebook</p>
+        </div>
+
+        
+    </div>
+    )
+}
+
+
+export default LoginInput
