44df7846e4b7b600bd2c48ef030d8c507fae2888
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2023-02-27 12:55:40 +0100

    page style

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,6 +4,8 @@ import phones from "../assets/phones.png";
 import screenshot1 from "../assets/screenshot1.png";
 import screenshot2 from "../assets/screenshot2.png";
 import screenshot3 from "../assets/screenshot3.png";
+import instagramlogo from '../assets/instagramlogo.png';
+import LoginInput from "../components/LoginInput";
 
 const LoginPage = () => {
   const [isNightMode, setIsNightMode] = useState(false);
@@ -18,14 +20,19 @@ const LoginPage = () => {
       <button className="mode-button" onClick={toggleNightMode}>
         Switch to {isNightMode ? "day" : "night"} mode
       </button>
-      <div className="input-img-container relative">
-        <img src={screenshot1} className="absolute n1" alt="" />
-        <img src={screenshot2} className="absolute n2" />
-        <img src={screenshot3} className="absolute n3" />
-      </div>
-      <div>
-        <form></form>
-      </div>
+        <div className="login-container">
+          <div className="input-img-container relative">
+            <img src={screenshot1} className="absolute n1" alt="" />
+            <img src={screenshot2} className="absolute n2" />
+            <img src={screenshot3} className="absolute n3" />
+          </div>
+          <div>
+            <form>
+              <img src={instagramlogo} alt="" />
+                <LoginInput/>
+            </form>
+          </div>
+        </div>
     </div>
   );
 };
